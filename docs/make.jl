0cc4ab8738aefaf3e02570578f95ac5e0515a160
Scott McIntire <scottrsm@gmail.com>
2024-04-03 16:40:44 -0400

    Added github doc and test actions

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,18 @@
-using Documenter, Wquantiles
+using Wquantiles
+import Pkg
 
-makedocs(sitename="Wquantile Docs")
+Pkg.add("Documenter")
+using Documenter
 
+makedocs(
+	sitename = "Wquantiles",
+	format = Documenter.HTML(),
+	modules = [Wquantiles]
+	)
+
+	# Documenter can also automatically deploy documentation to gh-pages.
+	# See "Hosting Documentation" and deploydocs() in the Documenter manual
+	# for more information.
+	deploydocs(
+		repo = "github.com/scottrsm/Wquantiles.jl.git"
+	)
